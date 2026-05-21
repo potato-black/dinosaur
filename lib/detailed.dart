352964dd795fdd25a5c4a42869bc617dbb565475
potato-black <gateball7@gmail.com>
2026-05-21 13:29:12 +0900

    2026-05-21 FEAT: 화면전환 구성

--- lib/detailed.dart
+++ lib/detailed.dart
@@ -8,10 +8,114 @@ class Detailed extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("챔피언맘 세부페이지"),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.green,
         centerTitle: true,
       ),
-      body: Text("챔피언맘 세부페이지", style: TextStyle(fontSize: 20)),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.green,
+            width: double.infinity,
+            height: 200,
+            child: Center(child: Image.asset("images/di1.png", width: 120)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "시대",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      "백악기 후기(6800만년 전)",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "시대",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      "백악기 후기(6800만년 전)",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "시대",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      "백악기 후기(6800만년 전)",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "시대",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      "백악기 후기(6800만년 전)",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "설명",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "T-Rex는 가장 잘 알려진 공룡 중 하나로,"
+                  "강력한 턱과 날카로운 이빨을 가진 포식자였습니다...",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
